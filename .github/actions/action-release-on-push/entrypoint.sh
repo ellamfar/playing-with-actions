15d441fd3a8af7099e6c53c95872b9414cfc439d
ellamfar <108862968+ellamfar@users.noreply.github.com>
2022-10-31 11:04:23 +0100

    create entrypoint.sh
    script to execute git tag and git push

---
+++ .github/actions/action-release-on-push/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+tag="$1"
+message="$2"
+
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+git tag -a "${tag}" -m "${message}"
+git push origin "${tag}"
